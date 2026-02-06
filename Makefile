e01e8d5150a9e0592f1ca872910fd02d5603d227
bezZzier <pontzonelu@gmail.com>
2026-02-06 13:53:50 +0200

    made the window appear also removed the test hello world

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	g++ main.cpp -o bezzZier -lsfml-graphics -lsfml-window -lsfml-system
+	./bezzZier
--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
-#include<iostream>
+// includes
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
 using namespace std;
 
+int screenx = 800;
+int screeny = 600;
+
 int main() {
-    cout << "hello world i hope this shows";
+    sf::RenderWindow window(sf::VideoMode(screenx, screeny), "BezZzier");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::White);
+        window.display();
+    }
+
     return 0;
 }
