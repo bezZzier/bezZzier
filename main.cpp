c2addcdadc567045b35f0f90c7874c2ed5e50c55
bezZzier <pontzonelu@gmail.com>
2026-02-06 18:08:36 +0200

    made dots placeable

--- main.cpp
+++ main.cpp
@@ -1,47 +1,55 @@
-// includes
+    // includes
 
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <vector>
+    #include <iostream>
+    #include <SFML/Graphics.hpp>
+    #include <vector>
 
-using namespace std;
+    using namespace std;
 
-// variables
+    // variables
 
-int screenx = 800;
-int screeny = 600;
+    int screenx = 800;
+    int screeny = 600;
 
-vector<sf::Vector2f> points = { {0.f, 0.f}, {100.f, 100.f} };
+    sf::Vector2f temp = {0, 0};
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(screenx, screeny), "BezZzier");
+    vector<sf::Vector2f> points = { {0.f, 0.f}, {100.f, 100.f} };
 
-    while (window.isOpen())
-    {
-        window.clear(sf::Color::White);
-        
-        for (size_t i = 0; i < points.size(); i++)
+    int main() {
+        sf::RenderWindow window(sf::VideoMode(screenx, screeny), "BezZzier");
+
+        while (window.isOpen())
         {
-            sf::CircleShape shape(10.f);
-            shape.setFillColor(sf::Color::Black);
-            shape.setPosition(points[i]);
-            window.draw(shape);
+            window.clear(sf::Color::White);
+
+            for (size_t i = 0; i < points.size(); i++)
+            {
+                sf::CircleShape shape(10.f);
+                shape.setFillColor(sf::Color::Black);
+                shape.setPosition(points[i]);
+                window.draw(shape);
+            }
+
+            // display window here
+
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+
+                if(event.type == sf::Event::MouseButtonPressed) {
+                    temp = sf::Vector2f(sf::Mouse::getPosition(window));
+                    std::cout << "x: " << to_string(temp.x) << " y: " << to_string(temp.y) << endl;
+                    points.push_back({temp.x, temp.y});
+                }
+            }
+
+            // reset window
+
+            window.display();
+            
         }
 
-        // display window here
-
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // reset window
-
-        window.display();
-        
+        return 0;
     }
-
-    return 0;
-}
