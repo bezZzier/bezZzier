868f4c87bd61c2a8451d6f6644c8934b8b93fa11
bezZzier <pontzonelu@gmail.com>
2026-02-06 18:26:05 +0200

    placing points draws lines between them

--- main.cpp
+++ main.cpp
@@ -13,43 +13,51 @@
 
     sf::Vector2f temp = {0, 0};
 
-    vector<sf::Vector2f> points = { {0.f, 0.f}, {100.f, 100.f} };
+    vector<sf::Vector2f> points = {};
 
     int main() {
-        sf::RenderWindow window(sf::VideoMode(screenx, screeny), "BezZzier");
+    sf::RenderWindow window(sf::VideoMode(screenx, screeny), "BezZzier");
 
-        while (window.isOpen())
+    while (window.isOpen())
+    {
+        window.clear(sf::Color::White);
+
+        // 1. Prepare the line path
+        sf::VertexArray line(sf::LineStrip, points.size());
+        for (size_t i = 0; i < points.size(); i++)
         {
-            window.clear(sf::Color::White);
-
-            for (size_t i = 0; i < points.size(); i++)
-            {
-                sf::CircleShape shape(10.f);
-                shape.setFillColor(sf::Color::Black);
-                shape.setPosition(points[i]);
-                window.draw(shape);
-            }
-
-            // display window here
-
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                if (event.type == sf::Event::Closed)
-                    window.close();
-
-                if(event.type == sf::Event::MouseButtonPressed) {
-                    temp = sf::Vector2f(sf::Mouse::getPosition(window));
-                    std::cout << "x: " << to_string(temp.x) << " y: " << to_string(temp.y) << endl;
-                    points.push_back({temp.x, temp.y});
-                }
-            }
-
-            // reset window
-
-            window.display();
-            
+            line[i].position = points[i];
+            line[i].color = sf::Color::Black;
         }
 
-        return 0;
+        // 2. Draw the line path once
+        window.draw(line);
+
+        // 3. Draw the circles on top
+        for (size_t i = 0; i < points.size(); i++)
+        {
+            sf::CircleShape point(10.f);
+            point.setFillColor(sf::Color::Black);
+            // Centering the circle on the line point
+            point.setPosition(points[i].x - 10.f, points[i].y - 10.f);
+            window.draw(point);
+        }
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if(event.type == sf::Event::MouseButtonPressed) {
+                temp = sf::Vector2f(sf::Mouse::getPosition(window));
+                cout << "x: " << temp.x << " y: " << temp.y << endl;
+                points.push_back(temp);
+            }
+        }
+
+        window.display();
     }
+
+    return 0;
+}
